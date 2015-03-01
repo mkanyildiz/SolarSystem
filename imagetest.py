acf66b5725f78e9fecbeb50c7b0bbe9e0aa18463
Marlene Dorfinger <mdorfinger@student.tgm.ac.at>
2015-03-01 21:30:15 +0100

    image laden mit pillow
    :penguin:

---
+++ imagetest.py
@@ -0,0 +1,21 @@
+from PIL import Image
+
+try:
+    im = Image.open("test.jpg") #image laden
+    print(im.format,im.size,im.mode) #infos ueber das bild
+    im.show() #image zeigen
+except:
+    print ("Unable to load image")
+
+#im.show()
+
+#hier wird ein bestimmter Bereich ausgeschnitten
+#box = (100, 100, 400, 400) #bereich angeben
+#region = im.crop(box) #ausschneiden
+#region.show()
+
+#hier wird der ausgschnittene Bereich um 180 grad gedreht wieder eingefügt
+#region = region.transpose(Image.ROTATE_180) #rotation
+#im.paste(region, box) #einfuegen
+
+#im.show()
