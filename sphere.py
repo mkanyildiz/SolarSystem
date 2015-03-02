7ca00d2a03108019e6a3c796c9f6ac9f6e43d6a3
mkanyildiz <mkanyildiz@hotmail.com>
2015-03-02 12:34:39 +0100

    rotation gefixed

--- sphere.py
+++ sphere.py
@@ -1,104 +1,146 @@
-from OpenGL.GLUT import *
-from OpenGL.GLU import *
-from OpenGL.GL import *
-import sys
 
-name = 'ball_glut'
+import math
+from OpenGL.raw.GLUT import glutSolidSphere
+import pygame
+
+from pygame.locals import *
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+from random import randint
+
+
+verticies = (
+    (1,     -1, -1),
+    (1,     1,  -1),
+    (-1,    1,  -1),
+    (-1,    -1, -1),
+    (1,     -1, 1),
+    (1,     1,  1),
+    (-1,    -1, 1),
+    (-1,    1,  1)
+)
+
+edges = (
+    (0,1),
+    (0,3),
+    (0,4),
+    (2,1),
+    (2,3),
+    (2,7),
+    (6,3),
+    (6,4),
+    (6,7),
+    (5,1),
+    (5,4),
+    (5,7)
+)
+
+surfaces = (
+    (0,1,2,3),
+    (3,2,7,6),
+    (6,7,5,4),
+    (4,5,1,0),
+    (1,5,7,2),
+    (4,0,3,6)
+)
+
+colors = (
+    (1,0,0),
+    (0,1,0),
+    (0,0,1),
+    (0,1,0),
+    (1,1,1),
+    (0,1,1),
+    (1,0,0),
+    (0,1,0),
+    (0,0,1),
+    (1,0,0),
+    (1,1,1),
+    (0,1,1)
+)
+
+def Cube():
+    glBegin(GL_QUADS)
+    x = 0
+    for surface in surfaces:
+        x = 0
+        for vertex in surface:
+            x+=1
+            glColor3fv(colors[x])
+            glVertex3fv(verticies[vertex])
+
+    glEnd()
+
+    glBegin(GL_LINES)
+    for edge in edges:
+        for vertex in edge:
+            glVertex3fv(verticies[vertex])
+    glEnd()
+
+def Sphere(radius):
+
+    sphere = gluNewQuadric()
+    gluQuadricDrawStyle(sphere,GLU_LINE);
+    gluSphere(sphere,radius,20,20)
+    #glutWireSphere(2,100,20)
 
 def main():
-    glutInit(sys.argv)
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
-    glutInitWindowSize(1000,1000)
-    glutCreateWindow(b'name')
+    pygame.init()
+    display = (800,600)
+    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+
+    gluPerspective(45, (display[0]/display[1]), 0.1, 200.0)
+
+    glTranslatef(0,0, -50)
+
+    glRotatef(25, 2, 1, 0)
+    glTranslatef(5,1,0.0)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    glTranslatef(-0.5,0,0)
+                if event.key == pygame.K_RIGHT:
+                    glTranslatef(0.5,0,0)
+                if event.key == pygame.K_UP:
+                    glTranslatef(0,0.5,0)
+                if event.key == pygame.K_DOWN:
+                    glTranslatef(0,-0.5,0)
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 4:
+                    glTranslatef(0,0,1.0)
+                if event.button == 5:
+                    glTranslatef(0,0,-1.0)
 
 
-    glClearColor(0.,0.,0.,1.)
-    glShadeModel(GL_SMOOTH)
-    glEnable(GL_CULL_FACE)
-    glEnable(GL_DEPTH_TEST)
-    glEnable(GL_LIGHTING)
-    lightZeroPosition = [0.,0.,10.,1.]
-    lightZeroColor = [0.8,1.0,0.8,1.0] #green tinged
-    glLightfv(GL_LIGHT0, GL_POSITION, lightZeroPosition)
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, lightZeroColor)
-    glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, 0.1)
-    glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 0.05)
-    glEnable(GL_LIGHT0)
+        glTranslatef(-10,0,0)
+        glRotatef(0.5, 0, 0, 1)
+        glTranslatef(10,0,0.0)
 
-    #glutDisplayFunc(display)
-    glutDisplayFunc(displaySun)
+        glPushMatrix()
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        glColor3f(0, 0, 1)
+        Sphere(1)
+        glColor3f(1, 1, 0)
+        glTranslatef(-10, 0, 0)
+        Sphere(2)
+        glPopMatrix()
 
-    glMatrixMode(GL_PROJECTION)
-    gluPerspective(100.,1.,1.,800.)
-    glMatrixMode(GL_MODELVIEW)
-    gluLookAt(0,0,12,
-              0,0,0,
-              0,1,0)
-    glPushMatrix()
-    glutMainLoop()
-    return
+        """glPushMatrix()
+        glRotatef(-0.5, 0, 0, 1)
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        Sphere()
+        glColor3f(0, 0, 1)
+        glPopMatrix()"""
 
-def display():
+        pygame.display.flip()
+        pygame.time.wait(10)
 
-    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-
-    color = [1.0,0.,1.,1.]
-
-
-    glPushMatrix
-    glTranslatef(5,0,0)
-    glRotatef(0.1, 0, 0, 1)
-    glTranslatef(-5,0,0.0)
-    glMaterialfv(GL_FRONT,GL_DIFFUSE,color)
-    glutWireSphere(1,10,20)
-    glPopMatrix
-    glutPostRedisplay()
-    glutSwapBuffers()
-    return
-
-def displaySun():
-    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-
-    color = [1.0,0.,1.,1.]
-
-
-    T = glutGet(GLUT_ELAPSED_TIME) / 1000
-    glPushMatrix
-    glMaterialfv(GL_FRONT,GL_DIFFUSE,color)
-    glRotatef(5 * T, 0, 1, 0)
-    glColor3f(1, 1, 0)
-    glutWireSphere(2, 20, 20)
-    glPopMatrix
-
-    glPushMatrix
-    glRotatef(90 * T, 0, 1, 0)
-    glTranslatef(5, 0, 0)
-    glRotatef(40 * T, 0, 1, 0)
-    glColor3f(1, 0, 0)
-    glutWireSphere(0.6, 10, 10)
-    glPopMatrix
-
-    glPushMatrix
-    glRotatef(60 * T, 0, 1, 0)
-    glTranslatef(-3, 0, 9)
-    glRotatef(50 * T, 0, 1, 0)
-    glColor3f(0, 1, 0)
-    glutWireSphere(1, 16, 16)
-
-    glPushMatrix
-    glRotatef(360 * T, 0, 1, 0)
-    glTranslatef(-1.7, 0, 0)
-    glRotatef(50 * T, 0, 1, 0)
-    glColor3f(0, 0, 1)
-    glutWireSphere(0.4, 10, 10)
-    glPopMatrix
-
-    glPopMatrix
-    glutPostRedisplay
-    glutSwapBuffers()
-    return
-
-
-
-
-if __name__ == '__main__': main()
+main()
