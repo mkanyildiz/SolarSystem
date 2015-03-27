a3fbedc46c2c225399d8d68e0f50a0cafa28b705
Marlene Dorfinger <mdorfinger@student.tgm.ac.at>
2015-03-27 18:10:44 +0100

    Steuerung erklärung > beschleunigung hinzu

--- start.py
+++ start.py
@@ -11,21 +11,22 @@ class Start:
     Diese Klasse startet das ganze Programm.
     """
 
-    root = tkinter.Tk() # tkinter braucht man für den Splashscreen
+    root = tkinter.Tk()  # tkinter braucht man für den Splashscreen
 
     s = Splashscreen(root, "./textures/splash.png", 2) # Splashscreen wird aufgerufen, Bild wird mitgegeben
-    s.__enter__() # Enter Methode
-    s.__exit__() # Exit Methode
+    s.__enter__()  # Enter Methode
+    s.__exit__()  # Exit Methode
 
-    root.withdraw() # schliesst das Tkinter Fenster, das sich automatisch öffnet
+    root.withdraw()  # schliesst das Tkinter Fenster, das sich automatisch öffnet
 
     # Diese messagebox zeigt sich bevor das Programm startet, wenn man ok klickt, startet das Programm.
     messagebox.showinfo("Steuerung", "W ... Ansicht ändern \n T ... Textur ein/aus \n F ... Licht aus \n O ... Licht an"
-                        "\n Mausrad drehen ... Rein und Rauszoomen \n \n Wird auch in der Konsole gezeigt")
+                        "\n Mausrad drehen ... Rein und Rauszoomen \n \n Wird auch in der Konsole gezeigt \n Linke und"
+                        "Rechte Pfeiltaste ... Beschleunigung in die angegebene Richtung")
 
     # Da die Infobox dann für immer verschwindet, geben wir es auch in der Konsole aus. 
-    print("Steuerung: \n W ... Ansicht ändern \n T ... Textur ein/aus \n F ... Licht aus \n O ... Licht an "
-          "\n Mausrad drehen ... Rein und Rauszoomen")
+    print("Steuerung: \n W ... Ansicht ändern \n T ... Textur ein/aus \n F ... Licht aus \n O ... Licht an \n Mausrad"
+          "drehen ... Rein und Rauszoomen \n Linke und Rechte Pfeiltaste ... Beschleunigung in die angegebene Richtung")
 
-    so = Sonnensystem() # Das Sonnensystem wird aufgerufen
-    so.main() # und ausgeführt
+    so = Sonnensystem()  # Das Sonnensystem wird aufgerufen
+    so.main()  # und ausgeführt
