29559ef6d93aa1dd51c2ad3af017d0b9be7a2e4f
mkanyildiz <mkanyildiz@hotmail.com>
2015-04-18 16:58:29 +0200

    doku

---
+++ .tox/docs/Scripts/pybabel-script.py
@@ -0,0 +1,10 @@
+#!C:\Users\Muhammed5\Documents\GitHub\SolarSystem\.tox\docs\Scripts\python.EXE
+# EASY-INSTALL-ENTRY-SCRIPT: 'Babel==1.3','console_scripts','pybabel'
+__requires__ = 'Babel==1.3'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('Babel==1.3', 'console_scripts', 'pybabel')()
+    )
